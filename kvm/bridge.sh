9ef08956497370858e12cd650252812da5757f02
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-09 17:15:49 +0200

    kvm/bridge.sh: add ip configuration
    Configure an IP address on the kvm bridge.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/bridge.sh
+++ kvm/bridge.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+IP_TOOL=/usr/bin/ip
 BRIDGE=$1
+IP=$2
 
 # create bridge
-ip link add name "$BRIDGE" type bridge
+$IP_TOOL link add name "$BRIDGE" type bridge
 
 # make sure bridge is up
-ip link set "$BRIDGE" up
+$IP_TOOL link set "$BRIDGE" up
 
 # set promiscuous mode
-ip link set "$BRIDGE" promisc on
+$IP_TOOL link set "$BRIDGE" promisc on
+
+# add IP address to bridge
+$IP_TOOL address add "$IP" dev "$BRIDGE"
--- kvm/start.sh
+++ kvm/start.sh
@@ -4,7 +4,7 @@ BR_SCRIPT=./bridge.sh
 VM_SCRIPT=./kvm.sh
 
 # start vm bridge
-$BR_SCRIPT vmbr0
+$BR_SCRIPT vmbr0 10.10.10.1/24
 
 # start vm(s)
 $VM_SCRIPT vm0.qcow2 vmtap0
