81419a96f3a35a647b3d6083f10016ac21a6db29
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-09 17:10:37 +0200

    fixup: add qemu-ifup.sh changes
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/qemu-ifup.sh
+++ kvm/qemu-ifup.sh
@@ -4,11 +4,6 @@ IP=/usr/bin/ip
 BRIDGE=vmbr0
 TAP=$1
 
-# make sure bridge exists
-ip link add name $BRIDGE type bridge
-ip link set $BRIDGE up
-ip link set $BRIDGE promisc on
-
 # add tap interface to bridge
 $IP link set "$TAP" up
 $IP link set "$TAP" promisc on
