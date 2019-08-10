6267473440f250277b4d788cb702f0919e836b36
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-10 21:53:08 +0200

    kvm/start.sh: add variables for IPs and MACs
    Add variables for bridge and VM IPs and MAC addresses.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/start.sh
+++ kvm/start.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+# scripts
 BR_SCRIPT=./bridge.sh
 DNSMASQ_SCRIPT=./dnsmasq.sh
 NAT_SCRIPT=./nat.sh
 VM_SCRIPT=./kvm.sh
 
+# bridge config
+BRIDGE_PREFIX=10.10.10.0/24
+BRIDGE_IP=10.10.10.1/24
+
+# dhcp config
+DYNIP_START=10.10.10.20
+DYNIP_END=10.10.10.254
+
+# VM config
+VM0_MAC=52:54:00:00:00:02
+VM0_IP=10.10.10.2
+
 # start vm bridge
-$BR_SCRIPT vmbr0 10.10.10.1/24
+$BR_SCRIPT vmbr0 $BRIDGE_IP
 
 # start dnsmasq on bridge interface
-$DNSMASQ_SCRIPT vmbr0 10.10.10.20,10.10.10.254 \
-	52:54:00:00:00:02,10.10.10.2
+$DNSMASQ_SCRIPT vmbr0 $DYNIP_START,$DYNIP_END \
+	$VM0_MAC,$VM0_IP
 
 # enable nat and ip forwarding on bridge interface
-$NAT_SCRIPT vmbr0 10.10.10.0/24
+$NAT_SCRIPT vmbr0 $BRIDGE_PREFIX
 
 # start vm(s)
-$VM_SCRIPT 0 vm0.qcow2 vmtap0 52:54:00:00:00:02
+$VM_SCRIPT 0 vm0.qcow2 vmtap0 $VM0_MAC
