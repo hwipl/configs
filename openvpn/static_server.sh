b5d17d943a0c650da4686d7e6d92742ecb819efa
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-13 15:03:46 +0200

    update openvpn/static_server.sh
    Add keep alive, lower privileges, and daemon options.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- openvpn/static_server.sh
+++ openvpn/static_server.sh
@@ -7,6 +7,13 @@ KEY_FILE=$3
 LINES="# Server configuration
 dev tun
 ifconfig $SERVER_IP $CLIENT_IP
-secret $KEY_FILE"
+secret $KEY_FILE
+keepalive 10 60
+ping-timer-rem
+persist-tun
+persist-key
+user nobody
+group nobody
+daemon"
 
 echo "$LINES"
