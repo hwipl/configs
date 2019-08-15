f1f6c1aa8d3c8327503e1c5c9456d28d4420958b
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-15 14:40:26 +0200

    openvpn/pubkey_genclient.sh: add option support
    Add support for providing an option like "nopass" to the client
    certificate generation command.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- openvpn/pubkey_genclient.sh
+++ openvpn/pubkey_genclient.sh
@@ -2,13 +2,14 @@
 
 FOLDER=$1
 NAME=$2
+OPTS=$3
 
 EASYRSA=/usr/bin/easyrsa
 
 cd "$FOLDER" || exit
 
 # create client certificate
-$EASYRSA build-client-full "$NAME" 
+$EASYRSA build-client-full "$NAME" "$OPTS"
 
 # copy files to server's directory
 mkdir "$NAME"
