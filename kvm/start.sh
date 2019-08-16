c9048305c1d0c01361f398c8f15ceefb2712692e
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-16 20:21:42 +0200

    kvm/start.sh: add more VMs
    Add more VMs to start script.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/start.sh
+++ kvm/start.sh
@@ -17,16 +17,28 @@ DYNIP_END=10.10.10.254
 # VM config
 VM0_MAC=52:54:00:00:00:02
 VM0_IP=10.10.10.2
+VM1_MAC=52:54:00:00:00:03
+VM1_IP=10.10.10.3
+VM2_MAC=52:54:00:00:00:04
+VM2_IP=10.10.10.4
+VM3_MAC=52:54:00:00:00:05
+VM3_IP=10.10.10.5
 
 # start vm bridge
 $BR_SCRIPT vmbr0 $BRIDGE_IP
 
 # start dnsmasq on bridge interface
 $DNSMASQ_SCRIPT vmbr0 $DYNIP_START,$DYNIP_END \
-	$VM0_MAC,$VM0_IP
+	$VM0_MAC,$VM0_IP \
+	$VM1_MAC,$VM1_IP \
+	$VM2_MAC,$VM2_IP \
+	$VM3_MAC,$VM3_IP
 
 # enable nat and ip forwarding on bridge interface
 $NAT_SCRIPT vmbr0 $BRIDGE_PREFIX
 
 # start vm(s)
 $VM_SCRIPT 0 vm0.qcow2 vmtap0 $VM0_MAC
+$VM_SCRIPT 1 vm1.qcow2 vmtap1 $VM1_MAC
+$VM_SCRIPT 2 vm2.qcow2 vmtap2 $VM2_MAC
+$VM_SCRIPT 3 vm3.qcow2 vmtap3 $VM3_MAC
