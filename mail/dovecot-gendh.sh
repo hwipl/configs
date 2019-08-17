5f01da7f28b36674faf11842a6ea690be7822adb
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-17 18:20:38 +0200

    mail/dovecot-gendh.sh: check if DH file already exists
    Check if file with DH parameters already exists before creating it.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- mail/dovecot-gendh.sh
+++ mail/dovecot-gendh.sh
@@ -4,4 +4,8 @@ OPENSSL=/usr/bin/openssl
 DH_FILE=/etc/dovecot/dh.pem
 DH_BITS=4096
 
+if [ -f "$DH_FILE" ]; then
+	echo "$DH_FILE already exists."
+	exit
+fi
 $OPENSSL dhparam -out $DH_FILE $DH_BITS
