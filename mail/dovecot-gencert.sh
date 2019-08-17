36c3dfac4e64091c7b2651675737b3c9dfb1c144
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-17 18:19:32 +0200

    mail/dovecot-gencert.sh: check if certificate already exists
    Check if certificate already exists before creating it.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- mail/dovecot-gencert.sh
+++ mail/dovecot-gencert.sh
@@ -7,6 +7,11 @@ TRUST_ANCHOR=/etc/ca-certificates/trust-source/anchors/dovecot.crt
 CP=/usr/bin/cp
 TRUST=/usr/bin/trust
 
+if [ -f "$CERT" ]; then
+	echo "$CERT already exists."
+	exit
+fi
+
 $MKCERT
 $CP $CERT $TRUST_ANCHOR 
 $TRUST extract-compat
