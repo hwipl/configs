b4c3b03456b4500077d55091c2409b4c82caf2d7
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-21 10:51:31 +0200

    kvm/dnsmasq.sh: configure static routes
    Add support for configuring classless static routes on dhcp clients and
    use it in kvm/start.sh.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/dnsmasq.sh
+++ kvm/dnsmasq.sh
@@ -3,6 +3,7 @@
 DNSMASQ=/usr/bin/dnsmasq
 IF=$1
 IP_RANGE=$2
+ROUTES=$3
 
 # set command line
 CMD_LINE="$DNSMASQ"
@@ -11,8 +12,11 @@ CMD_LINE="$CMD_LINE --bind-interfaces"
 CMD_LINE="$CMD_LINE --except-interface=lo"
 CMD_LINE="$CMD_LINE --dhcp-range=$IP_RANGE"
 
+# configure static routes on dhcp clients
+CMD_LINE="$CMD_LINE --dhcp-option=option:classless-static-route,$ROUTES"
+
 # treat remaining arguments as dhcp-host definitions
-shift 2
+shift 3
 for i in "${@}"
 do
 	CMD_LINE="$CMD_LINE --dhcp-host=$i"
