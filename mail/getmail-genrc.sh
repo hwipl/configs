bf1667a414876c5128742ad1394476ec70a69afc
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-24 17:54:54 +0200

    add mail/getmail-genrc.sh
    Add mail/getmail-genrc.sh script for creating a getmail configuration
    file and putting it in the ~/.getmail folder.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ mail/getmail-genrc.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# command line arguments
+NAME=$1
+SERVER=$2
+USERNAME=$3
+PASSWORD=$4
+
+# path definitions
+CONF_PATH=~/.getmail
+LOG_PATH="$CONF_PATH/$NAME.log"
+
+DELIVER_PATH=/usr/lib/dovecot/deliver
+
+# template configuration
+CONFIG="# getmail configuration
+[retriever]
+type = SimplePOP3SSLRetriever
+server = $SERVER
+username = $USERNAME
+password = $PASSWORD
+
+[destination]
+type = MDA_external
+path = $DELIVER_PATH
+arguments = (\"-e\",)
+
+[options]
+read_all = false
+delete = false
+received = false
+delivered_to = false
+message_log = $LOG_PATH
+"
+
+# print usage if not all arguments are present
+if [ "$#" -lt 4 ]; then
+	echo "Usage:"
+	echo "    $0 <name> <server> <username> <password>"
+	echo "Arguments:"
+	echo "    name:     name of configuration"
+	echo "    server:   address of mail server"
+	echo "    username: username of account on mail server"
+	echo "    password: password of account on mail server"
+	exit
+fi
+
+# create config directory if necessary
+if [ ! -e "$CONF_PATH" ]; then
+	mkdir $CONF_PATH
+fi
+
+# check if config file already exists
+if [ -e "$CONF_PATH/$NAME" ]; then
+	echo "$CONF_PATH/$NAME already exists."
+	exit
+fi
+
+# write config to file
+echo "$CONFIG" > "$CONF_PATH/$NAME"
