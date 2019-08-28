ea15e39918edb1ca52bf1f811062f3db50c4a532
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-28 11:15:41 +0200

    add mail/dovecot-user.sh
    Add mail/dovecot-user.sh script for adding a user that belongs to the
    dovecot group. If the user already exists, add it to the dovecot group.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ mail/dovecot-user.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USER=$1
+
+DOVECOT_GROUP=dovecot
+
+ID=/usr/bin/id
+USERMOD=/usr/bin/usermod
+USERADD=/usr/bin/useradd
+
+if [ "$#" -lt 1 ]; then
+	echo "Usage:"
+	echo "    $0 <user>"
+	echo "Arguments:"
+	echo "    user: name of the user you want to add/modify"
+	exit
+fi
+
+user_exists=$($ID -u "$USER" > /dev/null 2>&1; echo $?)
+ 
+if [ "$user_exists" -eq 0 ]; then
+       	echo "Adding existing user $USER to dovecot group ($DOVECOT_GROUP)."
+	$USERMOD -a -G $DOVECOT_GROUP "$USER"
+else
+	echo "Creating new user $USER."
+	$USERADD -m -U -G $DOVECOT_GROUP "$USER"
+	echo "Do not forget to set a password with:"
+	echo "passwd $USER"
+fi
