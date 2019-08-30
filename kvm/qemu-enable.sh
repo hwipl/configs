b0c97b57cab33028c4db6c57177a06d0d99f9ffc
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-30 14:21:24 +0200

    add kvm/qemu-enable.sh
    Add kvm/qemu-enable.sh for copying all VM scripts as qemu service
    configuration (see qemu-service.sh) to a specified subdirectory (<name>)
    in /etc/conf.d/qemu.d. This way, the set of VMs can be started/enabled
    with systemctl qemu-custom@<name>.service.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ kvm/qemu-enable.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+NAME=$1
+
+CONF_DIR=/etc/conf.d/qemu.d/$NAME
+
+CP=/usr/bin/cp
+
+SCRIPTS="
+../net/add_route.sh
+../net/forward_port.sh
+bridge.sh
+dnsmasq.sh
+kvm.sh
+nat.sh
+qemu-ifdown.sh
+qemu-ifup.sh
+start.sh
+stop.sh" 
+
+if [ "$#" -lt 1 ]; then
+	echo "Usage:"
+	echo "    $1 <name>"
+	echo "Arguments:"
+	echo "    name: name of virtual machine(s) configuration"
+	exit
+fi
+
+if [ -e "$CONF_DIR" ]; then
+	echo "$CONF_DIR already exists."
+	exit
+fi
+
+mkdir -p "$CONF_DIR"
+
+for i in $SCRIPTS; do
+	echo "Copying $i to $CONF_DIR."
+	$CP "$i" "$CONF_DIR"
+done
+
+# correct paths in start.sh
+echo "Correcting script paths in $CONF_DIR/start.sh"
+sed -i "s|ADD_ROUTE=../net/add_route.sh|ADD_ROUTE=./add_route.sh|" \
+	"$CONF_DIR/start.sh"
+sed -i "s|FORWARD_PORT=../net/forward_port.sh|FORWARD_PORT=./forward_port.sh|" \
+	"$CONF_DIR/start.sh"
