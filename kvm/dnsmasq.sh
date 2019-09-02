e9569b1b317bca3aa38c57ae7b512961dd3914f6
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 14:53:42 +0200

    kvm/dnsmasq.sh: save pid of dnsmasq process
    Save the pid of the dnsmasq process in the tmp directory to make
    shutting down the process easier.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/dnsmasq.sh
+++ kvm/dnsmasq.sh
@@ -5,11 +5,16 @@ IF=$1
 IP_RANGE=$2
 ROUTES=$3
 
+# pid file
+PID_DIR=/tmp/qemu-VMs
+PID_FILE=$PID_DIR/dnsmasq-$IF.pid
+
 # set command line
 CMD_LINE="$DNSMASQ"
 CMD_LINE="$CMD_LINE --interface=$IF"
 CMD_LINE="$CMD_LINE --bind-interfaces"
 CMD_LINE="$CMD_LINE --except-interface=lo"
+CMD_LINE="$CMD_LINE --pid-file=$PID_FILE"
 CMD_LINE="$CMD_LINE --dhcp-range=$IP_RANGE"
 
 # configure static routes on dhcp clients
@@ -22,4 +27,13 @@ do
 	CMD_LINE="$CMD_LINE --dhcp-host=$i"
 done
 
+# make sure pid_dir and pid_file exist
+if [ ! -e "$PID_DIR" ]; then
+	mkdir "$PID_DIR"
+fi
+if [ ! -e "$PID_FILE" ]; then
+	touch "$PID_FILE"
+fi
+
+# run the command
 $CMD_LINE
