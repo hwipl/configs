28aa68406670a2dda4db0a756ff341ce2f19b76e
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 14:02:06 +0200

    kvm/stop.sh: disable the bridge
    Call the bridge.sh script with the stop command to disable the VM
    bridge interface.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/stop.sh
+++ kvm/stop.sh
@@ -4,9 +4,11 @@ SOCK_DIR=/tmp/qemu-VMs
 
 # scripts
 NAT_SCRIPT=./nat.sh
+BR_SCRIPT=./bridge.sh
 
 # bridge config
 BRIDGE_PREFIX=10.10.10.0/24
+BRIDGE_IP=10.10.10.1/24
 
 # stop all VMs that have a monitor socket file in SOCK_DIR
 for i in "$SOCK_DIR"/*.sock
@@ -17,3 +19,6 @@ done
 
 # disable nat and ip forwarding on bridge interface
 $NAT_SCRIPT vmbr0 $BRIDGE_PREFIX stop
+
+# stop vm bridge
+$BR_SCRIPT vmbr0 $BRIDGE_IP stop
