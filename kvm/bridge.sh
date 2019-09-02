3f9ab66ff45b40c8e6c0d90d51a0920997a25f35
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 13:41:14 +0200

    kvm/bridge.sh: add stop command
    Add stop command to bridge.sh script for stopping the bridge interface.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/bridge.sh
+++ kvm/bridge.sh
@@ -3,6 +3,29 @@
 IP_TOOL=/usr/bin/ip
 BRIDGE=$1
 IP=$2
+CMD=$3
+
+if [ "$CMD" == "stop" ]; then
+	# disable the bridge
+	echo "Disabling bridge $BRIDGE."
+
+	# delete IP address from bridge
+	$IP_TOOL address del "$IP" dev "$BRIDGE"
+
+	# set promiscuous mode off
+	$IP_TOOL link set "$BRIDGE" promisc off
+
+	# make sure bridge is down
+	$IP_TOOL link set "$BRIDGE" down
+
+	# delete bridge
+	$IP_TOOL link del name "$BRIDGE" type bridge
+
+	exit
+fi
+
+# enable the bridge
+echo "Enabling bridge $BRIDGE."
 
 # create bridge
 $IP_TOOL link add name "$BRIDGE" type bridge
