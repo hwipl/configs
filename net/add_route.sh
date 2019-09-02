4b7c1b8d4dfb33c1f66e2d91e4e8e600d499748a
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 14:20:24 +0200

    net/add_route.sh: add stop command
    Add "stop" command to add_route.sh to stop routing of packets of the
    specified route.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- net/add_route.sh
+++ net/add_route.sh
@@ -4,17 +4,26 @@
 PREFIX=$1
 GW=$2
 DEV=$3
+CMD=$4
 
 IP=/usr/bin/ip
 
 if [ "$#" -lt 3 ]; then
 	echo "Usage:"
-	echo "    $0 <prefix> <gateway> <dev>"
+	echo "    $0 <prefix> <gateway> <dev> [stop]"
 	echo "Arguments:"
 	echo "    prefix:  route this network prefix"
 	echo "    gateway: route via this gateway"
 	echo "    dev:     use this net device for route"
+	echo "    stop:    stop this routing rule"
 	exit
 fi
 
+if [ "$CMD" == "stop" ]; then
+	echo "Stopping to route $PREFIX via $GW on dev $DEV"
+	$IP route del "$PREFIX" via "$GW" dev "$DEV"
+	exit
+fi
+
+echo "Starting to route $PREFIX via $GW on dev $DEV"
 $IP route add "$PREFIX" via "$GW" dev "$DEV"
