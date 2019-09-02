48c0907f1088ebc87d7c1648e6b3f52a7c370f06
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 14:14:32 +0200

    net/forward_port.sh: add stop command
    Add "stop" command to forward_port.sh script for stopping the specified
    forwarding rule.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- net/forward_port.sh
+++ net/forward_port.sh
@@ -7,18 +7,36 @@ PORT=$1
 DEST_IP=$2
 DEST_PORT=$3
 
+# command, e.g., stop/delete this rule
+CMD=$4
+
 IPTABLES=/usr/bin/iptables
 
 if [ $# -lt 3 ]; then
 	echo "Usage:"
-	echo "    $0 <port> <dest_ip> <dest_port>"
+	echo "    $0 <port> <dest_ip> <dest_port> [stop]"
 	echo "Arguments:"
 	echo "    port:      external port"
 	echo "    dest_ip:   internal forwarding IP"
 	echo "    dest_port: internal forwarding port"
+	echo "    stop:      stop this forwarding rule"
 	exit
 fi
 
+if [ "$CMD" == "stop" ]; then
+	echo "Stopping to forward port $PORT to $DEST_IP:$DEST_PORT."
+	$IPTABLES \
+		-t nat \
+		-D PREROUTING \
+		-p udp \
+		--dport "$PORT" \
+		-j DNAT \
+		--to "$DEST_IP:$DEST_PORT"
+
+	exit
+fi
+
+echo "Starting to forward port $PORT to $DEST_IP:$DEST_PORT."
 $IPTABLES \
 	-t nat \
 	-A PREROUTING \
