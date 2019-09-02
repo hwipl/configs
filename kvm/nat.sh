7cdffff7f4abb6dbe651bed36324e6c8336af420
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 12:53:28 +0200

    kvm/nat.sh: add stop command
    Add "stop" command to the nat.sh script for disabling the NAT and
    routing settings.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/nat.sh
+++ kvm/nat.sh
@@ -4,6 +4,25 @@ SYSCTL=/usr/bin/sysctl
 IPTABLES=/usr/bin/iptables
 IF=$1
 IP=$2
+CMD=$3
+
+if [ "$CMD" == "stop" ]; then
+	# disable all NAT/routing settings
+	echo "Disabling NAT/routing settings."
+
+	# disable ip forwarding
+	$SYSCTL net.ipv4.ip_forward=0
+
+	# disable nat on specified interface for specified ip range
+	$IPTABLES -t nat -D POSTROUTING -s "$IP" -j MASQUERADE
+	$IPTABLES -D FORWARD -m conntrack --ctstate RELATED,ESTABLISHED \
+		-o "$IF" -d "$IP" -j ACCEPT
+	$IPTABLES -D FORWARD -i "$IF" -s "$IP" -j ACCEPT
+	exit
+fi
+
+# enable all NAT/routing settings
+echo "Enabling NAT/routing settings."
 
 # enable ip forwarding
 $SYSCTL net.ipv4.ip_forward=1
