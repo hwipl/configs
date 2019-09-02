9f1cbb6ddf789cbc73d96e021f8da6da689031f1
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-02 14:00:21 +0200

    kvm/stop.sh: disable NAT and routing
    Call the nat.sh script with the stop command to disable NAT and routing
    on the VM bridge.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kvm/stop.sh
+++ kvm/stop.sh
@@ -2,6 +2,12 @@
 
 SOCK_DIR=/tmp/qemu-VMs
 
+# scripts
+NAT_SCRIPT=./nat.sh
+
+# bridge config
+BRIDGE_PREFIX=10.10.10.0/24
+
 # stop all VMs that have a monitor socket file in SOCK_DIR
 for i in "$SOCK_DIR"/*.sock
 do
@@ -9,3 +15,5 @@ do
 	echo "system_powerdown" | nc -U "$i"
 done
 
+# disable nat and ip forwarding on bridge interface
+$NAT_SCRIPT vmbr0 $BRIDGE_PREFIX stop
