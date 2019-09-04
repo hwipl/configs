a65e73323cca0fbe7dc6e2c140890ce3dc6efad8
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-04 18:09:45 +0200

    mail/getmail-crontab.sh: fix typo
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- mail/getmail-crontab.sh
+++ mail/getmail-crontab.sh
@@ -11,7 +11,7 @@ if [ "$#" -lt 1 ]; then
 	echo "Usage:"
 	echo "    $1 <getmailrc> [getmailrc...]"
 	echo "Arguments:"
-	echo "    getmailrc: getmail configuration file in ~/getmail/"
+	echo "    getmailrc: getmail configuration file in ~/.getmail/"
 	exit
 fi
 
