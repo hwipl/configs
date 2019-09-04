5ea5f16ee67645aae596806b2b3cd28cbe36d5ec
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-04 18:25:32 +0200

    mail/dovecot.sh: add usage
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- mail/dovecot.sh
+++ mail/dovecot.sh
@@ -10,6 +10,15 @@ GENCERT=./dovecot-gencert.sh
 GENDH=./dovecot-gendh.sh
 GENCONFIG=./dovecot-genconfig.sh
 
+if [ "$#" -lt 2 ]; then
+	echo "Usage:"
+	echo "    $0 <common_name> <mail_address>"
+	echo "Arguments:"
+	echo "    common_name:  common name of server for SSL certificate"
+	echo "    mail_address: postmaster mail address for SSL certificate"
+	exit
+fi
+
 $GENSSLCNF "$COMMON_NAME" "$MAIL_ADDRESS"
 $GENCERT
 $GENDH
