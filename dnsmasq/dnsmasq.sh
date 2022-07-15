cc26549e45bb71f182567553b2f83705b91974ad
hwipl <33433250+hwipl@users.noreply.github.com>
2022-07-15 18:06:23 +0200

    add dnsmasq/dnsmasq.sh
    Add the dnsmasq/dnsmasq.sh script for running the dnsmasq dhcp server on
    a network interface with an ip range, routes, and host definitions.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ dnsmasq/dnsmasq.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# configuration
+IF=$2
+IP_RANGE=$3
+ROUTES=$4
+
+# dnsmasq
+DNSMASQ=/usr/bin/dnsmasq
+
+# pid file
+PID_DIR=/tmp/dnsmasq-$IF
+PID_FILE=$PID_DIR/dnsmasq-$IF.pid
+
+# usage
+USAGE="Usage:
+  $0 start <if> <ip_range> <routes> [host-defs]
+  $0 stop
+
+Arguments:
+  if:		network interface, e.g., eth0
+  ip_range:	first,last IP, e.g.:
+		192.168.1.20,192.168.1.50
+  routes:	comma-separated routes, e.g.:
+		0.0.0.0/0,192.168.1.1,192.168.2.0/24,192.168.1.1
+  host_defs:	space-separated MAC,IP host definitions, e.g.:
+		52:54:00:00:00:02,192.168.1.2 52:54:00:00:00:03,192.168.1.3
+Example:
+  $0 eth1 192.168.1.20,192.168.1.50 0.0.0.0/0,192.168.1.1 \\
+	52:54:00:00:00:02,192.168.1.2 \\
+	52:54:00:00:00:03,192.168.1.3
+"
+
+# start dnsmasq
+function start_dnsmasq {
+	# make sure parameters are there
+	if [ -z "$IF" ] || [ -z "$IP_RANGE" ] || [ -z "$ROUTES" ]; then
+		echo "$USAGE"
+		exit 1
+	fi
+
+	# set command line
+	CMD_LINE="$DNSMASQ"
+	CMD_LINE="$CMD_LINE --interface=$IF"
+	CMD_LINE="$CMD_LINE --bind-interfaces"
+	CMD_LINE="$CMD_LINE --except-interface=lo"
+	CMD_LINE="$CMD_LINE --pid-file=$PID_FILE"
+	CMD_LINE="$CMD_LINE --dhcp-range=$IP_RANGE"
+
+	# configure static routes on dhcp clients
+	CMD_LINE="$CMD_LINE --dhcp-option=option:classless-static-route,$ROUTES"
+
+	# treat remaining arguments as dhcp-host definitions
+	shift 4
+	for i in "${@}"
+	do
+		CMD_LINE="$CMD_LINE --dhcp-host=$i"
+	done
+
+	# make sure pid_dir and pid_file exist
+	if [ ! -e "$PID_DIR" ]; then
+		mkdir -p "$PID_DIR"
+	fi
+	if [ ! -e "$PID_FILE" ]; then
+		touch "$PID_FILE"
+	fi
+
+	# run the command
+	$CMD_LINE
+}
+
+# stop dnsmasq
+function stop_dnsmasq {
+	pid=$(cat PID_FILE)
+	if [ -z "$pid" ]; then
+		echo "could not get dnsmasq pid"
+		exit 1
+	fi
+	echo "Stopping dnsmasq process with pid $pid"
+	kill "$pid"
+}
+
+CMD=$1
+case $CMD in
+"start")
+	start_dnsmasq "$@"
+	;;
+"stop")
+	stop_dnsmasq
+	;;
+*)
+	echo "$USAGE"
+	;;
+esac
